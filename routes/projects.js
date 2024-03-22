a36b705f193663b3b269f46eb7ed0f078a7dbc43
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-03-22 15:38:33 -0400

    added signup route

--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
 
 const Project = require("../models/Project");
 const Task = require("../models/Task");
@@ -20,5 +21,119 @@ router.post("/", (req, res, next) => {
       res.json(err);
     });
 });
+
+router.get("/", (req, res, next) => {
+  Project.find()
+    .populate("tasks")
+    .then((foundProjects) => {
+      console.log("Found Projects ===>", foundProjects);
+      res.json(foundProjects);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.get("/details/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findById(projectId)
+    .populate("tasks")
+    .then((foundProject) => {
+      console.log("Found project ===>", foundProject);
+      res.json(foundProject);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.put("/update/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findByIdAndUpdate(projectId, req.body, { new: true })
+    .populate("tasks")
+    .then((updatedProject) => {
+      console.log("Updated project ====>", updatedProject);
+      res.json(updatedProject);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+router.delete("/delete/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findByIdAndDelete(projectId)
+    .then((deletedProject) => {
+      console.log("This is our deleted project", deletedProject);
+      let taskIds = [...deletedProject.tasks];
+      let taskDeletions = taskIds.map((task) => {
+        return Task.findByIdAndDelete(task);
+      });
+      Promise.allSettled(taskDeletions)
+        .then((deletedTasks) => {
+          console.log("Deleted Tasks ==>");
+          res.json({ deletedProject, deletedTasks });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.json(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+
+// router.delete("/delete/:projectId", (req, res, next) => {
+//   const { projectId } = req.params;
+
+//   if (!mongoose.Types.ObjectId.isValid(projectId)) {
+//     res.status(400).json({ message: "Specified id is not valid" });
+//     return;
+//   }
+
+//   Project.findByIdAndDelete(projectId)
+//     .then((deletedProject) => {
+//       console.log("This is our deleted project", deletedProject);
+
+//       Task.deleteMany({project: deletedProject._id})
+//         .then((deletedTasks) => {
+//           console.log("These are the deleted tasks")
+//           res.json({ deletedProject, deletedTasks})
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//           res.json(err);
+//         });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.json(err);
+//     })
+    
+// });
 
 module.exports = router;
